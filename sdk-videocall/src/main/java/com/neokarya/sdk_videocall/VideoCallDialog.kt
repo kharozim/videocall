9f0e4555b4946330feb0a6ef6225a045dbcf784b
kharozim <kharozim.wrk@gmail.com>
2025-11-10 15:12:06 +0700

    Feat : - add licence - adjustment ui - add theme material 3 in dialog - clean up code

--- sdk-videocall/src/main/java/com/neokarya/sdk_videocall/VideoCallDialog.kt
+++ sdk-videocall/src/main/java/com/neokarya/sdk_videocall/VideoCallDialog.kt
@@ -1,10 +1,20 @@
 package com.neokarya.sdk_videocall
 
+import android.Manifest
 import android.app.Dialog
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
@@ -32,6 +42,10 @@ class VideoCallDialog {
   }
 
   class Builder(private val fragmentManager: FragmentManager) : BottomSheetDialogFragment() {
+    companion object {
+      private const val PERMISSION_REQ_ID = 22
+    }
+
     private lateinit var binding: DialogWebviewBinding
 
     private val factory by lazy {
@@ -40,6 +54,8 @@ class VideoCallDialog {
     }
     private val viewModel: VideoCallViewModel by viewModels { factory }
     private var urlVideoCall: String = ""
+
+    override fun getTheme(): Int = R.style.Theme_Material3_Light_BottomSheetDialog
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
       val dialog = super.onCreateDialog(savedInstanceState)
@@ -50,6 +66,7 @@ class VideoCallDialog {
 
         bottomSheet?.let {
           val behavior = BottomSheetBehavior.from(it)
+
           // 1. Atur ketinggian View agar MATCH_PARENT
           it.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
 
@@ -72,6 +89,7 @@ class VideoCallDialog {
       binding = DialogWebviewBinding.inflate(inflater, container, false)
 
 
+      setupWebView()
       setView()
 
       return binding.root
@@ -93,14 +111,23 @@ class VideoCallDialog {
 
               is StateUI.Success -> {
                 binding.btnCallRequest.text = "Sukses ${state.data.size}"
+                binding.lRoom.root.isVisible = true
+                binding.lForm.isVisible = false
+                binding.lRoom.webView.loadUrl(binding.edtName.text.toString().trim())
               }
             }
           }
         }
-
       }
-      binding.btnCallEnd.setOnClickListener {
+
+      binding.btnClose.setOnClickListener {
         dismiss()
+      }
+
+      binding.lRoom.btnCallEnd.setOnClickListener {
+        binding.lRoom.webView.loadUrl("")
+        binding.lRoom.root.isVisible = false
+        binding.lForm.isVisible = true
       }
     }
 
@@ -113,6 +140,138 @@ class VideoCallDialog {
     fun init(urlVideoCall: String): Builder {
       this.urlVideoCall = urlVideoCall
       return this
+    }
+
+
+    private fun setupWebView() {
+      binding.lRoom.webView.apply {
+        val client = CustomWebViewClient()
+        settings.javaScriptEnabled = true
+        webViewClient = client
+        webChromeClient = object : WebChromeClient() {
+          override fun onPermissionRequest(request: PermissionRequest?) {
+            Log.d("TAG", "onPermissionRequest: $request")
+            checkAndRequestPermissions(request)
+          }
+
+        }
+      }
+    }
+
+
+    // Menyimpan objek PermissionRequest sementara
+    private var pendingPermissionRequest: PermissionRequest? = null
+
+    private fun checkAndRequestPermissions(request: PermissionRequest?) {
+      pendingPermissionRequest = request
+
+      // Mengidentifikasi sumber daya yang diminta (kamera, mikrofon)
+      val requestedResources = request?.resources
+
+      // Cek apakah izin kamera atau mikrofon diminta
+      var needsCameraPermission = false
+      var needsAudioPermission = false
+      for (resource in requestedResources.orEmpty()) {
+        Log.d("TAG", "checkAndRequestPermissions: $resource")
+        if (resource == PermissionRequest.RESOURCE_VIDEO_CAPTURE) {
+          needsCameraPermission = true
+        } else if (resource == PermissionRequest.RESOURCE_AUDIO_CAPTURE) {
+          needsAudioPermission = true
+        }
+      }
+
+      val permissionsToRequest = mutableListOf<String>()
+
+      if (needsCameraPermission && ContextCompat.checkSelfPermission(
+          requireContext(),
+          Manifest.permission.CAMERA
+        ) != PackageManager.PERMISSION_GRANTED
+      ) {
+        permissionsToRequest.add(Manifest.permission.CAMERA)
+      }
+
+      if (needsAudioPermission && ContextCompat.checkSelfPermission(
+          requireContext(),
+          Manifest.permission.RECORD_AUDIO
+        ) != PackageManager.PERMISSION_GRANTED
+      ) {
+        permissionsToRequest.add(Manifest.permission.RECORD_AUDIO)
+      }
+
+      if (permissionsToRequest.isNotEmpty()) {
+        // Meminta izin kepada pengguna
+        ActivityCompat.requestPermissions(
+          requireActivity(),
+          permissionsToRequest.toTypedArray(),
+          PERMISSION_REQ_ID
+        )
+      } else {
+        // Izin sudah diberikan sebelumnya, langsung izinkan permintaan WebView
+        request?.grant(requestedResources)
+        pendingPermissionRequest = null
+      }
+    }
+
+    // --- Hasil Permintaan Izin Android ---
+
+    override fun onRequestPermissionsResult(
+      requestCode: Int,
+      permissions: Array<String>,
+      grantResults: IntArray,
+    ) {
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+      if (requestCode == PERMISSION_REQ_ID && pendingPermissionRequest != null) {
+
+        val grantedResources = mutableListOf<String>()
+
+        // Cek hasil untuk setiap izin
+        for (i in permissions.indices) {
+          Log.d("TAG", "onRequestPermissionsResult: ${grantResults[i]} ${permissions[i]}")
+          if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+            if (permissions[i] == Manifest.permission.CAMERA) {
+              grantedResources.add(PermissionRequest.RESOURCE_VIDEO_CAPTURE)
+            } else if (permissions[i] == Manifest.permission.RECORD_AUDIO) {
+              grantedResources.add(PermissionRequest.RESOURCE_AUDIO_CAPTURE)
+            }
+          }
+        }
+
+        if (grantedResources.isNotEmpty()) {
+          // Beri izin di WebView untuk sumber daya yang disetujui
+          pendingPermissionRequest?.grant(grantedResources.toTypedArray())
+        } else {
+          // Tolak izin jika tidak ada yang disetujui
+          pendingPermissionRequest?.deny()
+        }
+
+        pendingPermissionRequest = null
+      }
+    }
+
+
+    // Class WebViewClient Kustom
+    private inner class CustomWebViewClient : WebViewClient() {
+
+      // Metode untuk API Level 24 (Nougat) ke atas
+      override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        val newUrl = request?.url.toString()
+//      handleUrlChange(newUrl)
+        Log.d("TAG", "shouldOverrideUrlLoading: $newUrl")
+        // Membiarkan WebView menangani pemuatan URL
+        return super.shouldOverrideUrlLoading(view, request)
+      }
+
+
+      // Metode opsional: Dipanggil saat pemuatan halaman dimulai, termasuk setelah redirect
+      override fun onPageStarted(view: WebView?, url: String?, favicon: android.graphics.Bitmap?) {
+        if (url != null) {
+          // Berguna untuk melacak URL yang telah dimuat
+//        handleUrlChange(url)
+          Log.d("TAG", "onPageStarted: $url")
+        }
+        super.onPageStarted(view, url, favicon)
+      }
     }
   }
 
--- sdk-videocall/src/main/java/com/neokarya/sdk_videocall/WebViewActivity.kt
+++ sdk-videocall/src/main/java/com/neokarya/sdk_videocall/WebViewActivity.kt
@@ -1,13 +1,30 @@
 package com.neokarya.sdk_videocall
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.neokarya.sdk_videocall.databinding.ActivityWebViewBinding
+import kotlin.collections.orEmpty
+import kotlin.toString
 
 class WebViewActivity : AppCompatActivity() {
+  companion object {
+    private const val PERMISSION_REQ_ID = 22
+  }
 
   private val binding by lazy { ActivityWebViewBinding.inflate(layoutInflater) }
 
@@ -19,6 +36,153 @@ class WebViewActivity : AppCompatActivity() {
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
       insets
+    }
+
+    getData()
+    setView()
+  }
+
+
+  private fun setView() {
+    binding.btnSearch.setOnClickListener {
+      val url = binding.edtUrl.text.toString().trim()
+      binding.webView.loadUrl(url)
+    }
+    binding.btnOpenBrowser.setOnClickListener {
+      val url = binding.edtUrl.text.toString().trim()
+      startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
+    }
+    binding.btnBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
+  }
+
+  private fun getData() {
+    binding.webView.apply {
+      val client = CustomWebViewClient()
+      settings.javaScriptEnabled = true
+      webViewClient = client
+      webChromeClient = object : WebChromeClient() {
+        override fun onPermissionRequest(request: PermissionRequest?) {
+          Log.d("TAG", "onPermissionRequest: $request")
+          checkAndRequestPermissions(request)
+        }
+
+      }
+    }
+  }
+
+
+  // Menyimpan objek PermissionRequest sementara
+  private var pendingPermissionRequest: PermissionRequest? = null
+
+  private fun checkAndRequestPermissions(request: PermissionRequest?) {
+    pendingPermissionRequest = request
+
+    // Mengidentifikasi sumber daya yang diminta (kamera, mikrofon)
+    val requestedResources = request?.resources
+
+    // Cek apakah izin kamera atau mikrofon diminta
+    var needsCameraPermission = false
+    var needsAudioPermission = false
+    for (resource in requestedResources.orEmpty()) {
+      Log.d("TAG", "checkAndRequestPermissions: $resource")
+      if (resource == PermissionRequest.RESOURCE_VIDEO_CAPTURE) {
+        needsCameraPermission = true
+      } else if (resource == PermissionRequest.RESOURCE_AUDIO_CAPTURE) {
+        needsAudioPermission = true
+      }
+    }
+
+    val permissionsToRequest = mutableListOf<String>()
+
+    if (needsCameraPermission && ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.CAMERA
+      ) != PackageManager.PERMISSION_GRANTED
+    ) {
+      permissionsToRequest.add(Manifest.permission.CAMERA)
+    }
+
+    if (needsAudioPermission && ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.RECORD_AUDIO
+      ) != PackageManager.PERMISSION_GRANTED
+    ) {
+      permissionsToRequest.add(Manifest.permission.RECORD_AUDIO)
+    }
+
+    if (permissionsToRequest.isNotEmpty()) {
+      // Meminta izin kepada pengguna
+      ActivityCompat.requestPermissions(
+        this,
+        permissionsToRequest.toTypedArray(),
+        PERMISSION_REQ_ID
+      )
+    } else {
+      // Izin sudah diberikan sebelumnya, langsung izinkan permintaan WebView
+      request?.grant(requestedResources)
+      pendingPermissionRequest = null
+    }
+  }
+
+  // --- Hasil Permintaan Izin Android ---
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<String>,
+    grantResults: IntArray,
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    if (requestCode == PERMISSION_REQ_ID && pendingPermissionRequest != null) {
+
+      val grantedResources = mutableListOf<String>()
+
+      // Cek hasil untuk setiap izin
+      for (i in permissions.indices) {
+        Log.d("TAG", "onRequestPermissionsResult: ${grantResults[i]} ${permissions[i]}")
+        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+          if (permissions[i] == Manifest.permission.CAMERA) {
+            grantedResources.add(PermissionRequest.RESOURCE_VIDEO_CAPTURE)
+          } else if (permissions[i] == Manifest.permission.RECORD_AUDIO) {
+            grantedResources.add(PermissionRequest.RESOURCE_AUDIO_CAPTURE)
+          }
+        }
+      }
+
+      if (grantedResources.isNotEmpty()) {
+        // Beri izin di WebView untuk sumber daya yang disetujui
+        pendingPermissionRequest?.grant(grantedResources.toTypedArray())
+      } else {
+        // Tolak izin jika tidak ada yang disetujui
+        pendingPermissionRequest?.deny()
+      }
+
+      pendingPermissionRequest = null
+    }
+  }
+
+
+  // Class WebViewClient Kustom
+  private inner class CustomWebViewClient : WebViewClient() {
+
+    // Metode untuk API Level 24 (Nougat) ke atas
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+      val newUrl = request?.url.toString()
+//      handleUrlChange(newUrl)
+      Log.d("TAG", "shouldOverrideUrlLoading: $newUrl")
+      // Membiarkan WebView menangani pemuatan URL
+      return super.shouldOverrideUrlLoading(view, request)
+    }
+
+
+    // Metode opsional: Dipanggil saat pemuatan halaman dimulai, termasuk setelah redirect
+    override fun onPageStarted(view: WebView?, url: String?, favicon: android.graphics.Bitmap?) {
+      if (url != null) {
+        // Berguna untuk melacak URL yang telah dimuat
+//        handleUrlChange(url)
+        Log.d("TAG", "onPageStarted: $url")
+      }
+      super.onPageStarted(view, url, favicon)
     }
   }
 }
